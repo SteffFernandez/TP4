0f6fc6b69cc407fa9be7be4e2d606136fcbb270b
Steff Jose Fernandez Solis <b32595@ecci.ucr.ac.cr>
2018-11-19 09:00:43 -0600

    arreglo de seleccion

--- main.cpp
+++ main.cpp
@@ -1,23 +1,22 @@
 #include <iostream>
 #include "gen.h"
-#include <time.h>
+
 
 using namespace std;
 
 int main()
 {
-   /* Gen * g=new Gen();
+    Gen * g=new Gen();
     for(int i=0; i<100; i++){
     g->cruzar();
     g->mutar();
     g->ordenarPorAptitud();
-    //g->seleccion();
+
+    g->seleccion();
     }
     for(int i=0; i<100; i++){
-        cout<<g->getIndividuo(i)->getAptitud()<<endl;
-    }*/
-    srand(time(0));
-    double i= rand()%2/rand()%101;
-    cout<<i<<endl;
+        cout<<i<<". "<<g->getIndividuo(i)->getAptitud()<<endl;
+    }
+
     return 0;
 }
